f6fabb88e3a539180e3b96997ceb540b4ca2f5a4
DreamtaleCore <dreamtalewind@gmail.com>
2019-10-27 16:31:43 +0800

    add test file

--- test.py
+++ test.py
@@ -2,12 +2,13 @@ from __future__ import print_function
 
 from tqdm import tqdm
 
-from utils import get_config
-from trainer import MUID_Trainer, UID_Trainer
+from utils import get_config, evaluation_matrix, save_mat
+from trainer import Trainer
 import argparse
 from torch.autograd import Variable
 import torchvision.utils as vutils
-import sys
+import numpy as np
+from data import read_mat as image_loader
 import torch
 import os
 from torchvision import transforms
@@ -15,19 +16,21 @@ from PIL import Image
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--configs', type=str, default='configs/teacher-student.yaml', help="net configuration")
-parser.add_argument('--input_dir', type=str, default='/home/ros/ws/nips19/datasets/intrinsic/MIT-inner-split/trainA',
+parser.add_argument('--input_dir', type=str, default='E:/MMSR/datasets/train_data/mat_bicLRx4',
                     help="input image path")
+parser.add_argument('--gt_dir', type=str, default='E:/MMSR/datasets/train_data/mat',
+                    help="ground truth image path, images need have the same name")
 parser.add_argument('--output_folder', type=str, default='id_mit_train-inner-opt',
                     help="output image path")
 parser.add_argument('--checkpoint', type=str, default='checkpoints/mit_inner-opt/gen_00440000.pt',
-                    help="checkpoint of MUID")
-parser.add_argument('--output_path', type=str, default='.', help="path for logs, checkpoints, and VGG model weight")
-parser.add_argument('--trainer', type=str, default='MUID', help="MUID|UID")
+                    help="checkpoint of pre-trained model")
+parser.add_argument('--save_top_k', type=int, default=3,
+                    help="save the top [k] best results")
 opts = parser.parse_args()
 
 IMG_EXTENSIONS = [
-    '.jpg', '.JPG', '.jpeg', '.JPEG',
-    '.png', '.PNG', '.ppm', '.PPM', '.bmp', '.BMP',
+    '.jpg', '.JPG', '.jpeg', '.JPEG', '.bmp', '.BMP',
+    '.png', '.PNG', '.ppm', '.PPM', '.mat', '.MAT'
 ]
 
 
@@ -35,11 +38,6 @@ def is_image_file(filename):
     return any(filename.endswith(extension) for extension in IMG_EXTENSIONS)
 
 
-wo_fea = 'wo_fea' in opts.checkpoint
-
-
-torch.manual_seed(opts.seed)
-torch.cuda.manual_seed(opts.seed)
 if not os.path.exists(opts.output_folder):
     os.makedirs(opts.output_folder)
 
@@ -49,14 +47,11 @@ config = get_config(opts.config)
 # Setup model and data loader
 # configs['vgg_model_path'] = opts.output_path
 # Setup model and data loader
-trainer = MUID_Trainer(config)
+trainer = Trainer(config)
 
 state_dict = torch.load(opts.checkpoint, map_location='cuda:0')
-trainer.gen_i.load_state_dict(state_dict['i'])
-trainer.gen_r.load_state_dict(state_dict['r'])
-trainer.gen_s.load_state_dict(state_dict['s'])
-trainer.fea_s.load_state_dict(state_dict['fs'])
-trainer.fea_m.load_state_dict(state_dict['fm'])
+trainer.student.load_state_dict(state_dict['student'])
+trainer.teacher.load_state_dict(state_dict['teacher'])
 
 trainer.cuda()
 trainer.eval()
@@ -66,39 +61,120 @@ if 'new_size' in config:
 else:
     new_size = config['new_size_i']
 
-intrinsic_image_decompose = trainer.inference
+SI_SR = trainer.forward
+
+test_loader = []
+if os.path.isdir(opts.input):
+    image_list = os.listdir(opts.input)
+    image_list = [x for x in image_list if is_image_file(x)]
+else:
+    with open(os.path.join(opts.input), 'r') as fid:
+        lines = fid.readlines()
+        image_list = [x.strip() for x in lines]
+for img_name in image_list:
+    name = os.path.basename(img_name)
+    if os.path.isfile(opts.input):
+        dir_input = os.path.dirname(opts.input)
+    else:
+        dir_input = opts.input
+    tmp_pwd = os.path.join(dir_input, img_name)
+    image_tf = tmp_pwd
+    test_loader.append((image_tf, {'name': [name]}))
+
+out_root = opts.output_folder
+if not os.path.exists(out_root):
+    os.makedirs(out_root)
+
+
+def gather_results(result_dict):
+    """
+    Convert the result to text
+    :param result_dict:
+    :return:
+    """
+    lines = []
+    lines.append(','.join([str(x) for x in result_dict.keys()]) + '\n')
+    for items in zip(result_dict.values()):
+        lines.append(','.join([str(x) for x in items]) + '\n')
+    return lines
+
 
 with torch.no_grad():
-    transform = transforms.Compose([transforms.Resize(new_size),
-                                    transforms.ToTensor(),
-                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-
-    image_paths = os.listdir(opts.input_dir)
-    image_paths = [x for x in image_paths if is_image_file(x)]
-    t_bar = tqdm(image_paths)
+    stu_results = {}
+    tea_results = {}
+    t_bar = tqdm(test_loader)
     t_bar.set_description('Processing')
-    for image_name in t_bar:
-        image_pwd = os.path.join(opts.input_dir, image_name)
+    for (image_in, targets) in t_bar:
+        t_bar = tqdm(test_loader)
+        img_name = targets['name'][0]
+        if 'name' not in stu_results:
+            stu_results['name'] = []
+        if 'name' not in tea_results:
+            tea_results['name'] = []
+        stu_results['name'].append(img_name)
+        tea_results['name'].append(img_name)
 
-        out_root = os.path.join(opts.output_folder, image_name.split('.')[0])
+        images_in = image_loader(path=image_in, phase=None, key='im_LR')
+        images_gt = image_loader(path=image_in.replace(opts.input_dir, opts.gt_dir), phase=None, key='im_LR')
 
-        if not os.path.exists(out_root):
-            os.makedirs(out_root)
+        images_in = torch.from_numpy(np.ascontiguousarray(np.transpose(images_in, (2, 0, 1)))).float()
+        images_in = images_in.unsqueeze(0).cuda().detach()
 
-        image = Variable(transform(Image.open(image_pwd).convert('RGB')).unsqueeze(0).cuda())
+        stu_pred, tea_pred = SI_SR(images_in)
+        stu_pred = np.transpose(stu_pred.detach().cpu().squeeze().numpy(), (1, 2, 0))
+        tea_pred = np.transpose(tea_pred.detach().cpu().squeeze().numpy(), (1, 2, 0))
 
-        # Start testing
-        im_reflect, im_shading = intrinsic_image_decompose(image, wo_fea)
-        im_reflect = (im_reflect + 1) / 2.
-        im_shading = (im_shading + 1) / 2.
+        eval_stu = evaluation_matrix(stu_pred, images_gt)
+        eval_tea = evaluation_matrix(tea_pred, images_gt)
 
-        path_reflect = os.path.join(out_root, 'output_r.jpg')
-        path_shading = os.path.join(out_root, 'output_s.jpg')
+        for k, v in eval_stu:
+            if k not in stu_results:
+                stu_results[k] = []
+            stu_results[k].append(v)
+        for k, v in eval_tea:
+            if k not in tea_results:
+                tea_results[k] = []
+            tea_results[k].append(v)
 
-        vutils.save_image(im_reflect.data, path_reflect, padding=0, normalize=True)
-        vutils.save_image(im_shading.data, path_shading, padding=0, normalize=True)
+    print('\n Compute Done.')
+    print('Saving the log information...')
+    stu_info = gather_results(stu_results)
+    tea_info = gather_results(tea_results)
 
-        if not opts.output_only:
-            # also save input images
-            vutils.save_image(image.data, os.path.join(out_root, 'input.jpg'), padding=0, normalize=True)
+    fid_stu = open(os.path.join(out_root, 'result_student.csv'), 'w')
+    fid_tea = open(os.path.join(out_root, 'result_teacher.csv'), 'w')
+    fid_stu.writelines(stu_info)
+    fid_tea.writelines(tea_info)
+    fid_stu.close()
+    fid_tea.close()
+
+    print('Saving the top-{} best results for student.'.format(opts.save_top_k))
+    eval_judgement = stu_results[list(stu_results.keys())[0]]
+    idxs = np.argsort(eval_judgement)
+    # if find the top-k minimal values, change `idxs[:opts.save_top_k]` to `idxs[-opts.save_top_k:]`
+    saving_names = stu_results['name'][idxs[:opts.save_top_k]]
+    t_bar = tqdm(saving_names)
+    t_bar.set_description('Saving')
+
+    for img_name in t_bar:
+        images_in = image_loader(path=image_in, phase=None, key='im_LR')
+        images_gt = image_loader(path=image_in.replace(opts.input_dir, opts.gt_dir), phase=None, key='im_LR')
+
+        images_in = torch.from_numpy(np.ascontiguousarray(np.transpose(images_in, (2, 0, 1)))).float()
+        images_in = images_in.unsqueeze(0).cuda().detach()
+
+        stu_pred, tea_pred = SI_SR(images_in)
+        stu_pred = np.transpose(stu_pred.detach().cpu().squeeze().numpy(), (1, 2, 0))
+        tea_pred = np.transpose(tea_pred.detach().cpu().squeeze().numpy(), (1, 2, 0))
+
+        image_base_name, post_fix = os.path.basename(img_name).split('.')[-2:]
+        stu_name = '{}-{}.{}'.format(image_base_name, 'student', post_fix)
+        tea_name = '{}-{}.{}'.format(image_base_name, 'teacher', post_fix)
+        gt_name  = '{}-{}.{}'.format(image_base_name, 'student', post_fix)
+
+        save_mat(os.path.join(out_root, stu_name), stu_pred)
+        save_mat(os.path.join(out_root, stu_name), tea_pred)
+        save_mat(os.path.join(out_root, gt_name), images_gt)
+
+    print('\nAll Done.')
 
